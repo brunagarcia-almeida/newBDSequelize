e17ae5348f4caa536a957a7e63359874aa10a771
Bruna Garcia de Almeida <73366397+brunagarcia-almeida@users.noreply.github.com>
2024-03-02 01:02:27 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const { Sequelize, Model, DataTypes } = require("sequelize");
+const sequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: "empresa.sqlite",
+});
+(async () => {
+  await sequelize.sync({ force: true });
+})();
